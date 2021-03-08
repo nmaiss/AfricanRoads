a2773be7c72497209827b819b5455564669f1665
nmaiss <nicolas.maissouradze@gmail.com>
2021-03-08 19:15:00 +0100

    animation

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -9,9 +9,29 @@
         bottom: 10px;
         position: absolute;
     }
+
+    .animation{
+      height:0px;
+      width:1px;
+      border-bottom:1px solid #000;
+
+      -webkit-animation: increase 3s;
+      -moz-animation:    increase 3s;
+      -o-animation:      increase 3s;
+      animation:         increase 3s;
+      animation-fill-mode: forwards;
+    }
+
+    @keyframes increase {
+    100% {
+        width: 100%;
+    }
+}
+
 </style>
 
 <div id="copyright">
-<hr>
+<div class="animation mb-1"></div>
+<div>
 African Roads By HERITAGES © 2021
 </div>
