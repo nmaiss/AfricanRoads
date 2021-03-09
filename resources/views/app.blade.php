002cdb5973b1c4c932a37d80cae1192e59385742
nmaiss <nicolas.maissouradze@gmail.com>
2021-03-09 20:11:49 +0100

    footer

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -87,7 +87,7 @@
         <footer class="footer mt-auto py-3">
           <div id="copyright">
               <div class="animation mb-1"></div>
-              <div class="mt-2">African Roads By HERITAGES © 2021</div>
+              <div class="mt-2">African Road By HERITAGES © 2021</div>
           </div>
         </footer>
         <script src="/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
