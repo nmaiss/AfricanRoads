cc56b0322b224be8540c1467e8b0103509e92cb5
nmaiss <nicolas.maissouradze@gmail.com>
2021-03-09 21:25:52 +0100

    footer

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -69,7 +69,7 @@
               position: absolute;
               bottom: 0;
               width: 100%;
-              height: 60px;
+              height: 100px;
             }
 
             a:hover{
